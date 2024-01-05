5a632979e6ebb2a9df9eed01ecdf18784efd4f70
humanist96 <humanist96@gmail.com>
2024-01-05 17:22:05 +0900

    Update stat.py

--- eda/stat.py
+++ eda/stat.py
@@ -21,7 +21,7 @@ prop = fm.FontProperties(fname=fpath)
 
 def twoMeans(total_df):
     total_df['month'] = total_df['DEAL_YMD'].dt.month
-    apt_df = total_df[(total_df['HOUSE_TYPE'] == '아파트') & (total_df['month'].isin([3, 4]))]
+    apt_df = total_df[(total_df['HOUSE_TYPE'] == '아파트') & (total_df['month'].isin([11, 12]))]
     st.markdown("### 집계 \n"
                 "- 3월과 4월의 아파트 가격을 비교한다.")
     ttest_df = round(apt_df.groupby('month')['OBJ_AMT'].agg(["mean", "std", "size"]), 1)
@@ -30,28 +30,28 @@ def twoMeans(total_df):
     st.markdown("###  서울시 통합 3월 vs 4월 차이 검정\n"
                 "- 3월과 4월의 아파트 평균 가격의 차이를 검정한다. \n"
                 "- 가설설정 \n"
-                "   + 귀무가설 : $H_{0}$: 3월과 4월의 아파트 평균 차이는 없다. \n"
-                "   + 대립가설 : $H_{1}$: 3월과 4월의 아파트 평균 차이는 있다. \n")
+                "   + 귀무가설 : $H_{0}$: 11월과 12월의 아파트 평균 차이는 없다. \n"
+                "   + 대립가설 : $H_{1}$: 11월과 12월의 아파트 평균 차이는 있다. \n")
 
     march_df = apt_df[apt_df['month'] == 3]
     april_df = apt_df[apt_df['month'] == 4]
     result = ttest(march_df['OBJ_AMT'], april_df['OBJ_AMT'], paired=False)
     st.dataframe(result, use_container_width=True)
-    st.markdown(f"- 확인결과 p-value 값이 {result['p-val'].values[0]} 이므로 $H_{0}$을 채택하여, 3월과 4월의 아파트 평균 차이는 없다.")
+    st.markdown(f"- 확인결과 p-value 값이 {result['p-val'].values[0]} 이므로 $H_{0}$을 채택하여, 11월과 12월의 아파트 평균 차이는 없다.")
 
     selected_sgg_nm = st.sidebar.selectbox("자치구명", sorted(total_df['SGG_NM'].unique()))
     st.markdown(f"### 서울시 {selected_sgg_nm} 3월 vs 4월 차이 검정\n"
                 "- 자치구를 선택하여 3월과 4월의 아파트 평균 차이가 있는지 확인하도록 한다.")
 
     sgg_df = apt_df[apt_df['SGG_NM'] == selected_sgg_nm]
-    sgg_march_df = sgg_df[sgg_df['month'] == 3]
-    sgg_april_df = sgg_df[sgg_df['month'] == 4]
+    sgg_march_df = sgg_df[sgg_df['month'] == 11]
+    sgg_april_df = sgg_df[sgg_df['month'] == 12]
     sgg_result = ttest(sgg_march_df['OBJ_AMT'], sgg_april_df['OBJ_AMT'], paired=False)
     st.dataframe(sgg_result, use_container_width=True)
     if sgg_result['p-val'].values[0] > 0.05:
-        st.markdown(f"- 확인결과 p-value 값이 {sgg_result['p-val'].values[0]} 이므로 $H_{0}$을 채택하여, 3월과 4월의 아파트 평균 차이는 없다.")
+        st.markdown(f"- 확인결과 p-value 값이 {sgg_result['p-val'].values[0]} 이므로 $H_{0}$을 채택하여, 11월과 12월의 아파트 평균 차이는 없다.")
     else:
-        st.markdown(f"- 확인결과 p-value 값이 {sgg_result['p-val'].values[0]} 이므로 $H_{1}$을 채택하여, 3월과 4월의 아파트 평균 차이는 있다.")
+        st.markdown(f"- 확인결과 p-value 값이 {sgg_result['p-val'].values[0]} 이므로 $H_{1}$을 채택하여, 11월과 12월의 아파트 평균 차이는 있다.")
 
     st.markdown(f"### 서울시 :blue[{selected_sgg_nm}] 3월 vs 4월 시각화", unsafe_allow_html=True)
     fig, ax = plt.subplots(figsize=(10, 3))
@@ -62,7 +62,7 @@ def twoMeans(total_df):
 
 def corrRelation(total_df):
     total_df['month'] = total_df['DEAL_YMD'].dt.month
-    apt_df = total_df[(total_df['HOUSE_TYPE'] == '아파트') & (total_df['month'].isin([3, 4]))]
+    apt_df = total_df[(total_df['HOUSE_TYPE'] == '아파트') & (total_df['month'].isin([11, 12]))]
     st.markdown("### 상관관계 분석을 위한 데이터 확인 \n"
                 "- 건물면적과 물건금액의 상관관계를 확인해보도록 한다. \n"
                 "- 먼저 추출된 데이터를 확인한다.")
@@ -111,7 +111,7 @@ def corrRelation(total_df):
 
 def regRession(total_df):
     total_df['month'] = total_df['DEAL_YMD'].dt.month
-    apt_df = total_df[(total_df['HOUSE_TYPE'] == '아파트') & (total_df['month'].isin([3, 4]))]
+    apt_df = total_df[(total_df['HOUSE_TYPE'] == '아파트') & (total_df['month'].isin([11, 12]))]
     corr_df = apt_df[['DEAL_YMD', 'OBJ_AMT', 'BLDG_AREA', 'SGG_NM', 'month']].reset_index(drop=True)
     selected_sgg_nm = st.sidebar.selectbox("자치구명", sorted(corr_df['SGG_NM'].unique()))
     selected_month = st.sidebar.selectbox("월", sorted(corr_df['month'].unique()))

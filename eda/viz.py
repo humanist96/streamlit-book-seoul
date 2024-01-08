24730424a3416f32e4464932f8c9d19e7a219bd5
정경석 <humanist96@gmail.com>
2024-01-08 10:20:15 +0900

    11, 12월달 데이터로 갱신

--- eda/viz.py
+++ eda/viz.py
@@ -9,7 +9,7 @@ import numpy as np
 def meanChart(total_df, sgg_nm):
     st.markdown("## 가구별 평균 가격 추세 \n")
     filtered_df = total_df[total_df['SGG_NM'] == sgg_nm]
-    filtered_df = filtered_df[filtered_df['DEAL_YMD'].between("2023-03-01", "2023-04-30")]
+    filtered_df = filtered_df[filtered_df['DEAL_YMD'].between("2023-11-01", "2023-12-30")]
     result = filtered_df.groupby(['DEAL_YMD', 'HOUSE_TYPE'])['OBJ_AMT'].agg('mean').reset_index()
 
     df1 = result[result['HOUSE_TYPE'] == '아파트']  # 아파트
@@ -59,7 +59,7 @@ def meanChart(total_df, sgg_nm):
 def cntChart(total_df, sgg_nm):
     st.markdown("## 가구별 거래 건수 추세 \n")
     filtered_df = total_df[total_df['SGG_NM'] == sgg_nm]
-    filtered_df = filtered_df[filtered_df['DEAL_YMD'].between("2023-03-01", "2023-04-30")]
+    filtered_df = filtered_df[filtered_df['DEAL_YMD'].between("2023-11-01", "2023-12-30")]
     result = filtered_df.groupby(['DEAL_YMD', 'HOUSE_TYPE'])['OBJ_AMT'].count().reset_index().rename(columns = {'OBJ_AMT' : '거래건수'})
 
     df1 = result[result['HOUSE_TYPE'] == '아파트']  # 아파트
@@ -108,7 +108,7 @@ def cntChart(total_df, sgg_nm):
 
 def barChart(total_df):
     st.markdown("### 지역별 평균 가격 막대 그래프")
-    month_selected = st.selectbox("월을 선택하세요.", [3, 4])
+    month_selected = st.selectbox("월을 선택하세요.", [11, 12])
     house_selected = st.selectbox("가구 유형을 선택하세요", total_df['HOUSE_TYPE'].unique())
     total_df['month'] = total_df['DEAL_YMD'].dt.month
     result = total_df[(total_df['month'] == month_selected) & (total_df['HOUSE_TYPE'] == house_selected)]
